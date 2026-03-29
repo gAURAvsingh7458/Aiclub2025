db3dc16b154d246561ad9cdaee1188ecc31214b3
gaurav singh <jnvgauravsingh@gmail.com>
2026-03-30 01:26:24 +0530

    fix: railway deployment configurations (node engine, host binding, db path)

--- server.js
+++ server.js
@@ -202,6 +202,6 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
